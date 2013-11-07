9914b05645b35ea09dbcb3718408796e136638fc
Brian Fields <brian@minervaproject.com>
2013-11-07 15:24:37 -0800

    Attempt to change Audio codec from PCMU to Opus

--- erizo/src/erizo/SdpInfo.cpp
+++ erizo/src/erizo/SdpInfo.cpp
@@ -62,7 +62,7 @@ namespace erizo {
     ulpfec.channels = 1;
     ulpfec.mediaType = VIDEO_TYPE;
     internalPayloadVector_.push_back(ulpfec);
-/*
+
     RtpMap opus;
     opus.payloadType = OPUS_48000_PT;
     opus.encodingName = "opus";
@@ -70,7 +70,7 @@ namespace erizo {
     opus.channels = 2;
     opus.mediaType = AUDIO_TYPE;
     internalPayloadVector_.push_back(opus);
-
+/*
     RtpMap isac16;
     isac16.payloadType = ISAC_16000_PT;
     isac16.encodingName = "ISAC";
@@ -86,7 +86,7 @@ namespace erizo {
     isac32.channels = 1;
     isac32.mediaType = AUDIO_TYPE;
     internalPayloadVector_.push_back(isac32);
-*/
+
     RtpMap pcmu;
     pcmu.payloadType = PCMU_8000_PT;
     pcmu.encodingName = "PCMU";
@@ -94,7 +94,7 @@ namespace erizo {
     pcmu.channels = 1;
     pcmu.mediaType = AUDIO_TYPE;
     internalPayloadVector_.push_back(pcmu);
-/*
+
     RtpMap pcma;
     pcma.payloadType = PCMA_8000_PT;
     pcma.encodingName = "PCMA";
