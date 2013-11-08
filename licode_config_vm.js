4a1df4b0d1dc8371640292d96dcc89da33281378
Brian Fields <brian@minervaproject.com>
2013-11-08 14:18:44 -0800

    Update to Licode's config for VMs

--- licode_config_vm.js
+++ licode_config_vm.js
@@ -6,10 +6,10 @@ config.erizoController = {};
 config.cloudProvider = {};
 config.erizo = {};
 
-config.minervaHost = "av-local.minervaproject.com";		
-
 config.rabbit.host = 'localhost';
 config.rabbit.port = 5672;
+
+config.minervaHost = "av-local.minervaproject.com";
 
 config.nuve.dataBaseURL = "localhost/nuvedb";
 config.nuve.superserviceID = '5265b9a26c002eae466693e8';
@@ -18,13 +18,20 @@ config.nuve.testErizoController = 'localhost:8080';
 
 //Use undefined to run clients without Stun 
 config.erizoController.stunServerUrl = 'stun:stun.l.google.com:19302';
+
+//Use undefined to run clients without Turn
+config.erizoController.turnServer = undefined;
+config.erizoController.turnServer.url = undefined;
+config.erizoController.turnServer.username = undefined;
+config.erizoController.turnServer.password = undefined;
+
 config.erizoController.warning_n_rooms = 15;
 config.erizoController.limit_n_rooms = 20;
 config.erizoController.interval_time_keepAlive = 1000;
 
 //STUN server IP address and port to be used by the server.
 //if '' is used, the address is discovered locally
-config.erizo.stunserver = 'stun:stun.l.google.com';
+config.erizo.stunserver = 'stun.l.google.com';
 config.erizo.stunport = 19302;
 
 //note, this won't work with all versions of libnice. With 0 all the available ports are used
@@ -37,4 +44,8 @@ config.cloudProvider.host = '';
 config.cloudProvider.accessKey = '';
 config.cloudProvider.secretAccessKey = '';
 
+// Roles to be used by services
+config.roles = {"presenter":["publish", "subscribe", "record"], "viewer":["subscribe"]};
+
+var module = module || {};
 module.exports = config;
