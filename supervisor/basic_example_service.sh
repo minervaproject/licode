3b658a2a7b19c12d1b4942c74da1fdf5d8d74e6d
Gene Hallman <gene@minervaproject.com>
2015-09-17 15:03:24 -0700

    make the basic example check for nuve started status

--- supervisor/basic_example_service.sh
+++ supervisor/basic_example_service.sh
@@ -9,5 +9,13 @@ popd > /dev/null
 
 EXTRAS=$ROOT/extras
 
+sudo supervisorctl status nuve | grep RUNNING > /dev/null
+nuve_status=$?
+if [ $nuve_status -ne 0 ]; then
+  echo "status was $nuve_status"
+  >&2 echo "Nuve not running, exiting"
+  exit 1
+fi
+
 cd $EXTRAS/basic_example
 node basicServer.js
