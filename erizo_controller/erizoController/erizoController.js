ea6efdb52d5241cb8fe7312411c29a7d88dc5e0e
Pedro Rodriguez <lodoyun@gmail.com>
2016-09-09 12:07:22 +0200

    Revert "Revert "Merge remote-tracking branch 'ging/master'""
    This reverts commit e24d8db39ef21390a8076b867c1bb33cbb26ab57.

--- erizo_controller/erizoController/erizoController.js
+++ erizo_controller/erizoController/erizoController.js
@@ -506,9 +506,11 @@ var listen = function () {
                         }
                         return;
                     } else if (signMess.type ==='failed'){
-                        log.warn("IceConnection Failed on publisher, removing " , id);
+                        log.warn("IceConnection Failed on publisher " , id, "alerting clients");
                         socket.emit('connection_failed',{type:'publish', streamId: id});
-                        socket.state = 'sleeping';
+                        //We're going to let the client disconnect let the client do it
+                        
+                        /* 
                         if (!socket.room.p2p) {
                             socket.room.controller.removePublisher(id);
                             if (GLOBAL.config.erizoController.report.session_events) {
@@ -524,6 +526,7 @@ var listen = function () {
                         if (index !== -1) {
                             socket.streams.splice(index, 1);
                         }
+                        */
                         return;
                     } else if (signMess.type === 'ready') {
                         st.status = PUBLISHER_READY;
