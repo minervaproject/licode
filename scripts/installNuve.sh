0e79209ba69d838b71b2503e1f71d9decf9d8cd4
Javier Cerviño <jcague@gmail.com>
2016-11-25 12:05:31 +0100

    Reduce conflicts with ging by applying custom config files

--- scripts/installNuve.sh
+++ scripts/installNuve.sh
@@ -64,18 +64,6 @@ populate_mongo(){
   replacement=s/_auto_generated_KEY_/${SERVKEY}/
   sed $replacement $BUILD_DIR/licode_1.js > $ROOT/licode_config.js
   rm $BUILD_DIR/licode_1.js
-
-  # This could be more secure, but it works!  The seminar app needs this service ID/KEY, so put them in a place it can pull of https.
-  echo "{ \"SUPERSERVICE_ID\": \"$SERVID\", \"SUPERSERVICE_KEY\": \"$SERVKEY\" }" > $PATHNAME/../extras/basic_example/public/nuveServiceConfig.py
-  cat $PATHNAME/../extras/basic_example/public/nuveServiceConfig.py > $PATHNAME/../extras/basic_example/public/nuveServiceConfig.json
-
-  # Write network-config file
-  mkdir -p $ROOT/licode_config;
-
-  if [ ! -f "$ROOT/licode_config/host.js" ]; then
-    cp $PATHNAME/host_default.js $ROOT/licode_config/host.js
-  fi
-
 }
 
 install_nuve
